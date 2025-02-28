3a176eed917834bc84ff65234f38dd9559a0109d
Bruno Schott <bruno.schott@hotmail.com>
2025-02-28 17:31:58 -0300

    Create operadoresmatematicos2.c

---
+++ operadoresmatematicos2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(){
+
+/*
+Atribuição simoples (=)
+Atribuição de soma (+=)
+Atribuição de subtração (-=)   
+Atribuição de multiplicação (*=)
+Atribuição de divisão (/=)
+Atribuição de módulo (%=)
+*/  
+
+int numero1 = 10, numero2, resultado;
+
+resultado = 10;
+printf("Resultado: %d\n", resultado);
+
+//resultado = resultado + 20
+resultado += 20;
+printf("Resultado: %d\n", resultado);
+
+//resultado  = resultado -1
+resultado -= numero1;
+
+//resultado = resultado * 5
+resultado *= 5;
+printf("Resultado: %d\n", resultado);
+
+//resultado = resultado / 2
+resultado /= 2;
+printf("Resultado: %d\n", resultado);
+
+
+
+
+    }
