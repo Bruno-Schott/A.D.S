ad7f94aaffb6b955df545103d382128a57e7d840
Bruno Schott <bruno.schott@hotmail.com>
2025-02-28 17:32:17 -0300

    Create operadoresmatematicos3.c

---
+++ operadoresmatematicos3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+
+/*
+
+Incremento (++) 
+Pré-Incremento (++a)
+Pós-Incremento (a++)    
+Decremento (--)
+Pré-Decremento (--a)
+Pós-Decremento (a--)
+
+*/
+
+int numero1 = 1, resultado;
+
+printf("Antes do Incremento: %d\n", numero1);
+numero1++;
+printf("Depois do Incremento: %d\n", numero1);
+numero1--;
+printf("Depois do Incremento: %d\n", numero1);
+
+
+
+
+}
